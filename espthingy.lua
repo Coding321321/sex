cff9ae5cf00e8eaee3d25e304116d2e4a5d1daf1
Coding321321 <35693740+Coding321321@users.noreply.github.com>
2022-04-27 21:37:33 +0300

    Update espthingy.lua

--- espthingy.lua
+++ espthingy.lua
@@ -15,16 +15,20 @@ local Create = Drawing.new()
 function espModule:CreateLine(Visible, Thickness, Color3, Transparency, HRP)
 	print(Visible, Thickness, Color3, Transparency, HRP)
 	local Line = Create("Line")
-	Line.Visible = self.Visible
-	Line.Thickness = self.Thickness
-	Line.Color = self.Color3
-	Line.Transparency = self.Transparency
+	Line.Visible = Visible
+	Line.Thickness = Thickness
+	Line.Color = Color3
+	Line.Transparency = Transparency
+	
+	game:GetService("RunService").RenderStepped:Connect(function()
 	local vector, OnScreen = camera:worldToViewportPoint(HRP.Position) -- and not v = player :Shrug:
-	if OnScreen then
-		Line.From = Vector2.new(camera.ViewportSize.X / 2, camera.ViewportSize.Y / 2)
-		Line.To = Vector2.new(Vector.x, Vector.Y)
-	end
-
+		if OnScreen then
+			Line.From = Vector2.new(camera.ViewportSize.X / 2, camera.ViewportSize.Y / 2)
+			Line.To = Vector2.new(Vector.x, Vector.Y)
+		end
+		else
+		Line.Visible = false	
+	end)
 
 end
 
