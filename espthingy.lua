2e1c90ad184a6df7cbbf273a34c7a4ca7f40cf25
Coding321321 <35693740+Coding321321@users.noreply.github.com>
2022-04-27 22:30:04 +0300

    Update espthingy.lua

--- espthingy.lua
+++ espthingy.lua
@@ -30,9 +30,16 @@ local Line = Drawing.new("Line")
 			Line.To = Vector2.new(Vector.X, Vector.Y)
 		end
 	
-        if Players[HRP.Parent.Name] then
+        HRP.Parent.Humanoid.Died:Connect(function()
             Line:Remove()
         end
+
+        game.Players.PlayerRemoved:Connect(function(plr)
+            if HRP.Parent.Name == plr.Name then
+                Line:Remove()
+            end
+        end)
+
     end)
 
 end
@@ -56,6 +63,64 @@ function espModule:CreateCircle(Visible, Thickness, Color3, Transparency, Filled
        	 	Circle.Position = Position
   	 end)
     
+end
+
+function espModule:CreatePlayerBox(Visible, Thickness, Color3, Transparency, Filled, HRP)
+	local Outline = Drawing.new("Square")
+    Outline.Visible = Visible
+    Outline.Color = Color3
+    Outline.Thickness = Thickness
+    Outline.Transparency = Transparency
+    Outline.Filled = Filled
+
+    local Box = Drawing.new("Square")
+    Box.Visible = Visible
+    Box.Color = Color3
+    Box.Thickness = Thickness
+    Box.Transparency = Transparency
+    Box.Filled = Filled
+
+    game:GetService"RunService".RenderStepped:Connect(function(dt)
+        if HRP.Parent and HRP.Parent:FindFirstChild"Humanoid" and HRP.Parent:FindFirstChild"HumanoidRootPart" and HRP.Parent.Name ~= game.Players.LocalPlayer.Name and HRP.Parent.Humanoid.Health > 0 then
+
+            local Head = HRP.Parent.Head
+            local HRPPosition, HRPVis = worldToViewportPoint(camera, HRP.Position)
+            local HeadPos = worldToViewportPoint(camera, Head.Position + Vector3.new(0, 0.5, 0))
+            local LegPos = worldToViewportPoint(camera, HRP.Position - Vector3.new(0, 5, 0))
+
+            local Vector, OnScreen = camera:worldToViewportPoint(HRP.Position) 
+
+            if OnScreen then
+                Outline.Size = Vector2.new(1000 / HRPPosition.Size.Z, HeadPos.Size.Y - LegPos)
+                Outline.Position = Vector2.new(HRPPosition.X - Outline.Size.X / 2, HRPPosition.Y - Outline.Size.Y / 2)
+
+                Box.Size = Vector2.new(1000 / HRPPosition.Size.Z, HeadPos.Size.Y - LegPos)
+                Box.Position = Vector2.new(HRPPosition.X - OutlBoxine.Size.X / 2, HRPPosition.Y - Box.Size.Y / 2)
+
+                local person = Players[v.Parent.Name]
+
+                if person.TeamColor ~= game.Players.LocalPlayer.TeamColor then
+                    Box.Color = Color3.fromRGB(255,0,0)
+                else
+                    Box.Color = Color3.fromRGB(50,205,50)
+                end
+
+            end
+
+            HRP.Parent.Humanoid.Died:Connect(function()
+                Outline:Remove()
+                Box:Remove()
+            end)
+
+            game.Players.PlayerRemoved:Connect(function(plr)
+                if HRP.Parent.Name == plr.Name then
+                    Line:Remove()
+                end
+            end)
+    
+
+        end
+    end)
 end
 
 
